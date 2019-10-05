526d2d74dfc63263386d23b29b2d3fa422ab3668
amberlee <amberleeha@gmail.com>
2019-10-05 10:18:53 -0700

    boiler plate for html/routes

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var bestFriend = [{
+    name: "Anna Wintour",
+    photo: "images/annaWintour.jpeg ",
+    scores: ["1", "2", "3", "4", "5"]
+}, {
+    name: "Virgil Abloh",
+    photo: "images/virgilAbloh.jpeg",
+    scores: ["1", "2", "3", "4", "5"]
+}, {
+    name: "Miuccia Prada",
+    photo: "images/miucciaPrada.jpeg",
+    scores: ["1", "2", "3", "4", "5"]
+}, {
+    name: "Alexander Wang",
+    photo: "images/alexanderWang.jpeg",
+    scores: ["1", "2", "3", "4", "5"]
+}, ]
+
+module.exports = bestFriend
