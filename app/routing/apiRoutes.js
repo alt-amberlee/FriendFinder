15a6c6bf1cadf2c9aee769c453771bc1586c34e6
amberlee <amberleeha@gmail.com>
2019-10-06 23:53:57 -0700

    survey html and api route updated with bug fixes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,41 @@ var friends = require('../data/friends.js')
 module.exports = function(app) {
     console.log(friends);
     app.get('/api/friends', function(req, res) {
-        console.log(friends)
+        // console.log(friends)
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
-        console.log(req.body)
-        res.json(req.body);
+        // console.log(req.body)
+        // res.json(req.body);
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        var userInput = req.body;
+        var userScores = userInput.scores;
+
+        var userName = userInput.name;
+        var userPhoto = userInput.photo;
+
+        var totalDifference = 0;
+
+        for (var i = 0; i < friends.length - 1; i++) {
+            console.log(friends[i].name);
+            totalDifference = 0;
+
+            for (var j = 0; j < 10; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        friends.push(userInput);
+
+        res.json(bestMatch);
     });
 }
