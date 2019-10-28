7a4a1e0ed3e156a228a695f05c9ba29b99fb02e7
amberlee <amberleeha@gmail.com>
2019-10-28 15:17:48 -0700

    finished logic for calculation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,42 +1,28 @@
-var friends = require('../data/friends.js')
-    // console.log(friends);
+var friends = require('../data/friends.js');
+
 module.exports = function(app) {
-    console.log(friends);
     app.get('/api/friends', function(req, res) {
-        // console.log(friends)
         res.json(friends);
     });
     app.post('/api/friends', function(req, res) {
-        // console.log(req.body)
-        // res.json(req.body);
-        var bestMatch = {
-            name: "",
-            photo: "",
-            difference: 20
-        };
-        var newFriend = req.body;
-        var newPhoto = req.body.newPhoto;
-        var newName = req.body.newName;
-        var newScores = req.body.scores;
-
-        var friendCalc = 0;
-
-        for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
-            friendCalc = 0;
-
-            for (var j = 0; j < friends[i].scores.length; j++) {
-                friendCalc += Math.abs(friends[i].scores[j] - newScores[j]);
-
-                if (friendCalc < bestMatch.difference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.difference = friendCalc;
-                }
-            }
+        friends.push(req.body);
+        res.json(friends);
+    });
+    app.post("/api/tables", function(req,res) {
+        if(tableData.length< 5) {
+            tableData.push(req.body);
+            res.json(true);
         }
-        friends.push(newFriend);
+        else {
+            waitlistData.push(req.body);
+            res.json(false);
+        }
+    });
 
-        res.json(bestMatch);
+    app.post("/api/clear", function(req,res) {
+        tableData.length = 0;
+        waitlistData.length = 0;
+
+        res.json({ ok: true })
     });
 }
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 // Configurate 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// Sets Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
